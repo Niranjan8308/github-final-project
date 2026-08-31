5ec722bbff651fb38d1208ffc182562bf6aea8bd
Niranjan8308 <niranjangovindgs@gmail.com>
2026-08-31 12:11:21 +0530

    Improve prompts and variable naming in script

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Simple Interest Calculator
-echo "Enter Principal:"
+
+echo "Enter Principal amount:"
 read p
+
 echo "Enter Rate of Interest:"
 read r
-echo "Enter Time (Years):"
+
+echo "Enter Time period in years:"
 read t
-s='expr $P \* $r \* $t / 100'
-echo "The Simple Interest is: $s"
+
+si=$(expr $p \* $r \* $t / 100)
+echo "The Simple Interest is: $si"
